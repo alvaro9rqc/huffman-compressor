80ba084c5e3c103b7fa30109cd1afa3eb7d8bdc7
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-06-25 08:11:43 -0500

    style: format cland style

--- include/io_tool.h
+++ include/io_tool.h
@@ -1,9 +1,10 @@
 #ifndef IO_TOOL_H
-#define IO_TOOL_H 
+#define IO_TOOL_H
 
 #include "huffman.h"
+#include "stdio.h"
 
-
-double io_read_bytes(Node* pq, char* file);
+double io_read_bytes(Node *pq, char *file);
+int io_save_code(FILE *file, char *filename, unsigned char **huff_code);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,22 +1,21 @@
-#include <string.h>
-#include <stdio.h>
 #include "compress.h"
-int main(int argc, char* argv[])  {
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[]) {
   //
-  if (argc < 3) { 
-    fprintf(stderr,"to comprees files: compress file1 file2 ... compresFile.cprs\n");
+  if (argc < 3) {
+    fprintf(stderr,
+            "to comprees files: compress file1 file2 ... compresFile.cprs\n");
     fprintf(stderr, "to decompress file: compress -d file1.cprs\n");
-    return  0;
+    return 0;
   }
-  if (strcmp( argv[1], "-d" )==0 || strcmp(argv[1], "-decode")==0) {
+  if (strcmp(argv[1], "-d") == 0 || strcmp(argv[1], "-decode") == 0) {
     printf("Descomprimir\n");
-  } else { 
-    //Compress
+  } else {
+    // second argument is compressed file name
     printf("Code: \n");
-    FILE* file = fopen(argv[argc-1], "w");
+    FILE *file = fopen(argv[argc - 1], "w");
     compress_encode_files(file, argc, argv);
     fclose(file);
   }
-
-
 }
