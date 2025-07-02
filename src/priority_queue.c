4cd7757a38f9cbb9b38127c40338cb68e6ff5ecf
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-07-02 05:23:39 -0500

    feat(pq): make a copy of nodes

--- src/priority_queue.c
+++ src/priority_queue.c
@@ -2,17 +2,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char pq_is_full(PriorityQueue *pq) { return pq->size == pq->capacity; }
 
-char pq_is_full(PriorityQueue *pq){
-  return pq->size == pq->capacity;
-}
+char pq_is_empty(PriorityQueue *pq) { return pq->arr[0] == NULL; }
 
-char pq_is_empty(PriorityQueue *pq){
-  return pq->arr[0] == NULL;
-}
-
-void pq_new(PriorityQueue* pq, Node* arr, int size){
-  pq->arr = (Node**) calloc(size, sizeof(Node*));
+void pq_new(PriorityQueue *pq, Node *arr, int size) {
+  pq->arr = (Node **)calloc(size, sizeof(Node *));
   pq->size = 0;
   pq->capacity = size;
   for (int i = 0; i < size; ++i) {
@@ -20,59 +15,68 @@ void pq_new(PriorityQueue* pq, Node* arr, int size){
   }
 }
 
-void pq_erase(PriorityQueue *pq){
-  fprintf(stderr, "Requiere implementation pq_erase\n");
-  exit(EXIT_FAILURE);
-}
+void pq_erase(PriorityQueue *pq) { free(pq->arr); }
 
-void pq_heapifyUp(PriorityQueue *pq, int idx){
+void pq_heapifyUp(PriorityQueue *pq, int idx) {
   int p;
-  while (idx > 0 && pq->arr[idx]->frequency < pq->arr[ p = (idx-1)>>2 ]->frequency) {
-    Node* t = pq->arr[idx];
-    pq->arr[idx]=pq->arr[p];
-    pq->arr[p]=t;
+  while (idx > 0 &&
+         pq->arr[idx]->frequency < pq->arr[p = (idx - 1) >> 2]->frequency) {
+    Node *t = pq->arr[idx];
+    pq->arr[idx] = pq->arr[p];
+    pq->arr[p] = t;
     idx = p;
   }
 }
 
-void pq_heapifyDown(PriorityQueue *pq, int idx){
-  while(2*idx + 1 < pq->size) {
-    int l = 2*idx + 1;
-    int r = l+1;
+void pq_heapifyDown(PriorityQueue *pq, int idx) {
+  while (2 * idx + 1 < pq->size) {
+    int l = 2 * idx + 1;
+    int r = l + 1;
     int lowest = idx;
-    if (l < pq->size  && pq->arr[l]->frequency < pq->arr[lowest]->frequency)
+    if (l < pq->size && pq->arr[l]->frequency < pq->arr[lowest]->frequency)
       lowest = l;
     if (r < pq->size && pq->arr[r]->frequency < pq->arr[lowest]->frequency)
       lowest = r;
-    if (lowest == idx) break;
-    Node* t = pq->arr[lowest];
+    if (lowest == idx)
+      break;
+    Node *t = pq->arr[lowest];
     pq->arr[lowest] = pq->arr[idx];
     pq->arr[idx] = t;
     idx = lowest;
   }
 }
 
-void pq_push(PriorityQueue* pq, Node* n) {
+void pq_push(PriorityQueue *pq, Node *n) {
   if (pq_is_full(pq) && pq->size != 0) {
     fprintf(stderr, "Error: PriorityQueue is full\n");
     exit(EXIT_FAILURE);
   }
+  // Copy
+  Node *copy = malloc(sizeof(Node));
+  if (copy == NULL) {
+    perror("malloc failed");
+    exit(EXIT_FAILURE);
+  }
+  *copy = *n;
   pq->arr[pq->size] = n;
   pq_heapifyUp(pq, pq->size++);
 }
 
-Node* pq_top(PriorityQueue* pq) {
+Node *pq_top(PriorityQueue *pq) {
   if (pq_is_empty(pq)) {
-    fprintf(stderr, "Error, se intentó extraer elemento de cola de prioridad vacía\n");
+    fprintf(stderr,
+            "Error, se intentó extraer elemento de cola de prioridad vacía\n");
     exit(EXIT_FAILURE);
   }
-  fprintf(stderr, "p: %p v: %d f: %f\n", pq->arr[0], pq->arr[0]->byte, pq->arr[0]->frequency);
+  fprintf(stderr, "p: %p v: %d f: %f\n", pq->arr[0], pq->arr[0]->byte,
+          pq->arr[0]->frequency);
   return pq->arr[0];
 }
 
-void pq_pop(PriorityQueue *pq){
+void pq_pop(PriorityQueue *pq) {
   if (pq_is_empty(pq)) {
-    fprintf(stderr, "Error, se intentó extraer elemento de cola de prioridad vacía\n");
+    fprintf(stderr,
+            "Error, se intentó extraer elemento de cola de prioridad vacía\n");
     exit(EXIT_FAILURE);
   }
   pq->arr[0] = pq->arr[--(pq->size)];
