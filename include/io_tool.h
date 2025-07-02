2fd73bb62e355c733d01c1492d35884629f306fe
alvaro9rqc <aquispecondo@unsa.edu.pe>
2025-07-02 05:20:41 -0500

    feat(io): add save code fuction declaration

--- include/io_tool.h
+++ include/io_tool.h
@@ -5,6 +5,9 @@
 #include "stdio.h"
 
 double io_read_bytes(Node *pq, char *file);
-int io_save_code(FILE *file, char *filename, unsigned char **huff_code);
+
+[[nodiscard("Handling error")]]
+int io_save_code(FILE *file, char *filename, unsigned char **huff_code,
+                 Node *root);
 
 #endif
